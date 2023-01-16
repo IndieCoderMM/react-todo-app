06f4505ba8dce3f7fa38f874013a1b183d4f69cf
hthantoo <hthant00chk@gmail.com>
2023-01-16 22:47:20 +0630

    Add humburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
   const links = [
     {
       id: 1,
@@ -14,13 +17,24 @@ const Navbar = () => {
       text: 'About',
     },
   ];
+  const toggleNav = () => setNavOpen((prev) => !prev);
+  const closeNav = () => setNavOpen(false);
   return (
     <nav className='navBar'>
-      <ul>
+      <button onClick={toggleNav}>
+        {navOpen ? (
+          <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+        ) : (
+          <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navOpen ? ' showMenu' : ''}`}>
         {links.map((link) => {
           return (
             <li key={link.id}>
-              <NavLink to={link.path}>{link.text}</NavLink>
+              <NavLink to={link.path} onClick={() => closeNav()}>
+                {link.text}
+              </NavLink>
             </li>
           );
         })}
